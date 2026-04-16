d90a41d3a9763387169b35df1aa287ad55a5cbbb
Mohammad-Roby <muhammadrooby@gmail.com>
2026-04-16 09:17:45 +0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -3,13 +3,13 @@ async function upload(){
   const form = new FormData();
   form.append('file', file);
 
-  await fetch('http://localhost:8787/upload',{method:'POST',body:form});
+  await fetch('https://cold-haze-26b9.muhammadrooby.workers.dev/upload',{method:'POST',body:form});
   alert('Uploaded');
 }
 
 async function check(){
   const kdtk = document.getElementById('kdtk').value;
-  const res = await fetch('http://localhost:8787/check?kdtk='+kdtk);
+  const res = await fetch('https://cold-haze-26b9.muhammadrooby.workers.dev/check?kdtk='+kdtk);
   const data = await res.json();
   document.getElementById('result').textContent = JSON.stringify(data,null,2);
 }
