c3dc388976328d65a39421c9a827978ca5e11369
Mohammad-Roby <muhammadrooby@gmail.com>
2026-04-16 09:28:54 +0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -7,24 +7,6 @@ async function upload(){
   alert('Uploaded');
 }
 
-async function check(){
-  const kdtk = document.getElementById('kdtk').value;
-
-  const res = await fetch('https://cold-haze-26b9.muhammadrooby.workers.dev/check?kdtk=' + kdtk);
-  const data = await res.json();
-
-  let output = "";
-
-  if(!data || !data.issues){
-    output = "Error mengambil data";
-  } else if(data.issues.length === 0){
-    output = "Semua kondisi normal";
-  } else {
-    output = "Masalah ditemukan:\n\n";
-    data.issues.forEach(i => {
-      output += "- " + i + "\n";
-    });
-  }
-
-  document.getElementById('result').textContent = output;
+function check(){
+  alert("JS jalan"); // TEST
 }
